2eea26585547cc53e701510baffbb817d48aa8b7
csturgeon <sturgeon.chris@gmail.com>
2022-11-29 10:10:28 +0000

    Modify mongoose connection consts

--- app.js
+++ app.js
@@ -16,7 +16,10 @@ var app = express();
 
 // setup mongoose connection
 const mongoose = require('mongoose');
+const dev_db_url =
+  'mongodb+srv://cooluser:coolpassword@cluster0.bsgsztp.mongodb.net/local_library?retryWrites=true';
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error'));
